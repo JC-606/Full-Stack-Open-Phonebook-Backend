f2e899263f3abe05f339ed46fdc61b79caaabf1f
jiachen-zhu-00 <jiachen.zhu@zing-x.studio>
2024-11-13 10:41:51 +0800

    exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+var morgan = require('morgan')
+
+morgan.token('data', (req, res) => JSON.stringify(req.body))
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
     { 
